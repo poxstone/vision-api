6063c6667d575f6d2818863744e6bf5a513138bf
developer <developer@domain.com>
2018-11-03 16:59:46 -0500

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+# INSTALLATION
+```bash
+pip install -r requirements.txt -t lib --upgrade;
+export GOOGLE_APPLICATION_CREDENTIALS="$(PWD)/poxgolden-358eea9146ff.json";
+```
---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append(sys.path[0] + '/lib/')
+print(sys.path)
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+# The name of the image file to annotate
+file_name = os.path.join(
+    os.path.dirname(__file__),
+    'resources/tomaco.jpg')
+
+# Loads the image into memory
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+
+image = types.Image(content=content)
+
+# Performs label detection on the image file
+response = client.label_detection(image=image)
+labels = response.label_annotations
+
+print('Labels:')
+for label in labels:
+    print(label.description)
