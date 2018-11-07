f120517dddbc738ebe83b4559b30e95be78ac7f7
developer <developer@domain.com>
2018-11-07 12:40:24 -0500

    add populate

--- app_vision/__init__.py
+++ app_vision/__init__.py
@@ -3,9 +3,10 @@ import json
 
 from flask import Flask
 from flask.templating import render_template
+from .models import FirestoreHelper
 
-from helpers import VisionHelper
-from constants import BUCKET, PROJECT_ID
+from .helpers import VisionHelper
+from .constants import BUCKET, PROJECT_ID
 
 
 # Run flask
@@ -41,7 +42,7 @@ def create_app(config, debug=False, testing=False, config_overrides=None):
 
     @app.route('/add/<user>', methods=['GET', 'POST'])
     def add(user):
-        from models import FirestoreHelper
+        from .models import FirestoreHelper
 
         fire_db = FirestoreHelper(PROJECT_ID)
         result_a = fire_db.addData(u'users', user, {
@@ -61,6 +62,23 @@ def create_app(config, debug=False, testing=False, config_overrides=None):
             docs_arr += u'{} => {}'.format(doc.id, doc.to_dict())
 
         return docs_arr
+
+    @app.route('/populate/')
+    def populate():
+        from .to_populate import ITEMS
+
+        fire_db = FirestoreHelper(PROJECT_ID)
+
+        for item in ITEMS:
+            result_a = fire_db.addData(u'nutrition', item['name'], {
+                "alternatives": item["alternatives"],
+                "nutrition": item["nutrition"]
+            })
+
+        # read data
+        docs = fire_db.getData(u'nutrition')
+
+        return str(docs)
 
     return app
 
---
+++ app_vision/to_populate.py
@@ -0,0 +1,47 @@
+ITEMS = [
+        {
+            "name": "banana",
+            "alternatives": ["banano", "platano"],
+            "nutrition": {
+                "fiber":"3.1 grams.",
+                "vitamin_b6": "22% of the RDI",
+                "vitamin_c": "17% of the RDI",
+                "manganese": "16% of the RDI",
+                "potassium": "12% of the RDI",
+                "magnesium": "8% of the RDI",
+                "folate": "6% of the RDI",
+                "copper": "5% of the RDI",
+                "riboflavin": "5% of the RDI"
+            }
+        },
+        {
+            "name": "tomato",
+            "alternatives": ["tomate", "tomacos"],
+            "nutrition": {
+                "fiber":"3.1 grams.",
+                "vitamin_b6": "22% of the RDI",
+                "vitamin_c": "17% of the RDI",
+                "manganese": "16% of the RDI",
+                "potassium": "12% of the RDI",
+                "magnesium": "8% of the RDI",
+                "folate": "6% of the RDI",
+                "copper": "5% of the RDI",
+                "riboflavin": "5% of the RDI"
+            }
+        },
+        {
+            "name": "carrot",
+            "alternatives": ["zanahoria", "carriot"],
+            "nutrition": {
+                "fiber":"3.1 grams.",
+                "vitamin_b6": "22% of the RDI",
+                "vitamin_c": "17% of the RDI",
+                "manganese": "16% of the RDI",
+                "potassium": "12% of the RDI",
+                "magnesium": "8% of the RDI",
+                "folate": "6% of the RDI",
+                "copper": "5% of the RDI",
+                "riboflavin": "5% of the RDI"
+            }
+        }
+    ]
